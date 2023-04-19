88639a1960469eea1ef3491704ea45d09ccda714
josheeen <therealjosh26@gmail.com>
2023-04-19 22:42:25 +0800

    done with prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine();
+
+        var num = -1;
+
+        //negative 1 because the last number that will be entered is zero
+        //zero will not be added for the total number that was entered
+        var numsEntered = -1; 
+
+        List<int> numbers = new List<int>();
+        while (num != 0)
+        {
+            Console.Write("Enter number: ");
+            num = int.Parse(Console.ReadLine());
+            numbers.Add(num);
+            numsEntered++;
+        }
+        float total = numbers.Sum();
+        float ave = total / numsEntered;
+        int max = numbers.Max();
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {ave}");
+        Console.WriteLine($"The largest number is: {max}");
+
     }
 }
